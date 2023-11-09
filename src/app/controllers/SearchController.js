b1bd6440a190c594d3d59a45b5d05c5b3f5e0e4b
VinhHuuTran <tranhuuvinh1109@gmail.com>
2023-11-09 22:45:01 +0700

    add api search teacher, center, all

--- src/app/controllers/SearchController.js
+++ src/app/controllers/SearchController.js
@@ -3,7 +3,7 @@ const Teacher = require("../models/Teacher");
 const Course = require("../models/Course");
 
 class SearchController {
-  // [GET] /search
+  // [GET] /search/all
   async search(req, res) {
     try {
       const query = req.params.query;
@@ -19,7 +19,6 @@ class SearchController {
         course_name: { $regex: query, $options: "i" },
       });
 
-      // Chờ các kết quả tìm kiếm từ các model
       const [centers, teachers, courses] = await Promise.all([
         centerPromise,
         teacherPromise,
@@ -37,6 +36,51 @@ class SearchController {
     } catch (error) {
       console.error(error);
       res.status(500).json({ error: "Search failed" });
+    }
+  }
+
+  // [GET] /search/center
+  async searchCenter(req, res) {
+    try {
+      const query = req.params.query;
+      const centerPromise = Center.find({
+        center_name: { $regex: query, $options: "i" },
+      });
+
+      const [centers] = await Promise.all([centerPromise]);
+
+      res.status(200).json({
+        message: "Search Center successful",
+        data: {
+          centers,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Search Center failed" });
+    }
+  }
+
+  // [GET] /search/teacher
+  async searchTeacher(req, res) {
+    try {
+      const query = req.params.query;
+
+      const teacherPromise = Teacher.find({
+        teacher_name: { $regex: query, $options: "i" },
+      });
+
+      const [teachers] = await Promise.all([teacherPromise]);
+
+      res.status(200).json({
+        message: "Search Teacher successful",
+        data: {
+          teachers,
+        },
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: "Search Teacher failed" });
     }
   }
 }
--- src/routes/RootRoute.js
+++ src/routes/RootRoute.js
@@ -3,7 +3,9 @@ const RootController = require("../app/controllers/RootController");
 const SearchController = require("../app/controllers/SearchController");
 const router = express.Router();
 
-router.get("/search/q=:query", SearchController.search);
+router.get("/search/all/q=:query", SearchController.search);
+router.get("/search/center/q=:query", SearchController.searchCenter);
+router.get("/search/teacher/q=:query", SearchController.searchTeacher);
 router.get("/", RootController.index);
 
 module.exports = router;
